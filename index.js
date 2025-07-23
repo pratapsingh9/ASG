2dcd8430b95dce724670719622ddbf8c0afc0d80
Your Name <you@example.com>
2025-07-23 17:20:31 +0530

    asdafa

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const express = require('express');
 
-
 function calculate(n) {
+  if (n <= 1) return 1;
   return 1 + calculate(n-1);
 }
 
 const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send("Hello World");
-})
+});
 
-app.get('/cpu/:n',(req,res)=> {
-  return res.send(calculate(req.params.n))
-})
+app.get('/cpu/:n', (req, res) => {
+  const result = calculate(parseInt(req.params.n));
+  return res.send(`Result: ${result}`);
+});
 
-app.get('/ip',(req,res)=> {
+app.get('/ip', (req, res) => {
   res.send(req.ip);
-})
+});
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
   res.send("About Page");
-})
+});
 
-app.get('/contact',(req,res)=>{
+app.get('/contact', (req, res) => {
   res.send("Contact Page");
-})
+});
 
-
-app.listen(3000,()=> {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
-})
+});
